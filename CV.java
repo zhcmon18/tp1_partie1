5a6681419a0a49a06e91a4ac93197432163aaad7
zhcmon18 <zhcmon18@gmail.com>
2018-02-02 19:06:09 -0500

    La premiere version de la classe CV avec les attribut et le constructeur

---
+++ CV.java
@@ -0,0 +1,20 @@
+
+public class CV {
+	 
+	private String nom;
+	  private String prenom;
+	  private String formation;
+	  private float experience;
+	  private String[] competences;
+	  private String attentes;
+
+
+	  public CV(String nom, String prenom, String formation, float experience, String[] competences, String attentes){
+	    this.nom = nom;
+	    this.prenom = prenom;
+	    this.formation = formation;
+		this.experience = experience;
+		this.competences = competences;
+		this.attentes = attentes;
+	 }
+}
